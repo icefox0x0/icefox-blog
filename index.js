9493f15c3cdc8d501bb3b805045913a07da9f6cd
icefox <88721186+icefox0@users.noreply.github.com>
2024-06-23 20:57:36 +0800

    更新辣

---
+++ index.js
@@ -0,0 +1,10 @@
+var input_passwd = prompt("drowssap eht epyt esaelP:");
+//check the person 
+if(input_passwd ==="013740x") 
+{
+  window.location.replace("home.html");
+}
+else
+{
+  alert("404");
+}
