1f95b55685c0484102e6b82f91cf29cda1b5c6c3
雪狸 <88721186+icefox0@users.noreply.github.com>
2024-08-07 22:09:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var input_passwd = prompt(":drowssap eht epyt esaelP");
 //check the person 
 if(input_passwd ==="013740x") 
 {
-  window.location.replace("home.html");
+  window.location.replace("./zh-cn/home.html");
 }
 else  if(input_passwd==="013740y"){
   window.location.replace("home2.html");
