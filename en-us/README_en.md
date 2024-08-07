87aa7840393bfb9fa44a4f618635bead6f041acc
雪狸 <88721186+icefox0@users.noreply.github.com>
2024-08-07 22:06:32 +0800

    Add files via upload

---
+++ en-us/README_en.md
@@ -0,0 +1,8 @@
+# Welcome!
+***
+
+[en-us](README_en.md)|[zh-cn](README.md)
+
+This project is built with GitHub Pages~
+
+You can have a look on this [site](https://icefox0.github.io//icefox-blog/) to find something that was created by me.
---
+++ zh-cn/index.js
@@ -0,0 +1,13 @@
+var input_passwd = prompt(":drowssap eht epyt esaelP");
+//check the person 
+if(input_passwd ==="013740x") 
+{
+  window.location.replace("home.html");
+}
+else  if(input_passwd==="013740y"){
+  window.location.replace("home2.html");
+}
+else  
+{
+  alert("404");
+}
